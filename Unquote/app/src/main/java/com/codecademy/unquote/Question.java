957d0d6505b35fdb0a6318421be26969374ac764
xavioo <rucahoxavi@gmail.com>
2022-01-24 12:34:06 +0200

    unquote

---
+++ Unquote/app/src/main/java/com/codecademy/unquote/Question.java
@@ -0,0 +1,29 @@
+package com.codecademy.unquote;
+
+public class Question {
+    int correctAnswer;
+    int playerAnswer;
+    int imageId;
+    String questionText;
+    String answer1;
+    String answer2;
+    String answer3;
+    String answer4;
+
+    public Question(int imageIdentifier, String questionString, String answerOne, String answerTwo,
+                    String answerThree, String answerFour, int correctAnswerIndex) {
+        imageId = imageIdentifier;
+        questionText = questionString;
+        answer1 = answerOne;
+        answer2 = answerTwo;
+        answer3 = answerThree;
+        answer4 = answerFour;
+        correctAnswer = correctAnswerIndex;
+        playerAnswer = -1;
+    }
+
+    boolean isCorrect() {
+        return correctAnswer == playerAnswer;
+    }
+}
+
